69fa00267e0fa20c4356692dac1d6547b2076955
marc4813 <mas1660@knights.ucf.edu>
2022-10-19 14:02:37 -0400

    Finishes lane detection and uses Haar cascade for car detection

--- src/laneDetect.py
+++ src/laneDetect.py
@@ -1,15 +1,14 @@
 # Library imports
-from statistics import mode
 import cv2
 import os
 import numpy as np
 
 # Grab image in a format that openCV can work with
-picpath = os.path.join("Pictures", "test_img.jpg")
-origimg = cv2.imread(picpath, cv2.IMREAD_UNCHANGED)
+picPath = os.path.join("Pictures", "test_img3.jpg")
+origImg = cv2.imread(picPath, cv2.IMREAD_UNCHANGED)
 
 # Apply a gaussian blur to smoothen out the picture. This removes small edges that we aren't really looking for. 
-img = cv2.GaussianBlur(origimg,(5,5),cv2.BORDER_DEFAULT)
+img = cv2.GaussianBlur(origImg,(5,5),cv2.BORDER_DEFAULT)
 
 # Convert to black and white image.
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -23,10 +22,11 @@ height, width = img.shape
 # Input points
 # Order: Top left, bottom left, bottom right, top right,
 A1 = [int(width*0.45),int(height*0.6)]
-B1 = [int(width*0.2),int(height*0.9)]
-C1 = [int(width*0.75),int(height*0.90)]
+B1 = [int(width*0.20),int(height*0.95)]
+C1 = [int(width*0.80),int(height*0.92)]
 D1 = [int(width*0.55),int(height*0.6)]
 inpts = [A1,B1,C1,D1]
+
 # Output points 
 A2 = [0,0]
 B2 = [0, height-1]
@@ -41,12 +41,40 @@ matrix = cv2.getPerspectiveTransform(inpts, outpts)
 
 warp = cv2.warpPerspective(img, matrix, (width,height))
 
-contours = []
+# Get edges
+edges = cv2.Canny(warp, 100,100)
 
-# Get contours
+# Inverse transform 
+invTrans = np.linalg.pinv(matrix)
+lanes = cv2.warpPerspective(warp, invTrans, (width, height))
 
-# Transform each pixel in the contour to the actual image
+# Change image color
+lanes = cv2.cvtColor(lanes, cv2.COLOR_GRAY2BGR)
 
+# Turn white lines into green lines 
+whiteHi = np.array([255,255,255])
+whiteLo = np.array([25,25,25])
 
-cv2.imshow('Image',warp)
+# Get white areas in image
+mask = cv2.inRange(lanes, whiteLo, whiteHi)
+
+# Replace white with green (blue, green, red) using boolean indexing
+lanes[mask>0]=(0,255,0)
+
+# Overlay lanes over original image 
+laneDetect = cv2.addWeighted(origImg, 0.6, lanes, 0.4,0)
+
+# Initialize haar cascade 
+carCascade = cv2.CascadeClassifier()
+paramPath = os.path.join("cascadeParams", "cars.xml")
+carCascade.load(paramPath)
+
+# Detect cars
+cars = carCascade.detectMultiScale(origImg)
+
+# draw bounding boxes (top left, bottom right)
+for (x,y,w,h) in cars:
+    cv2.rectangle(laneDetect, (x,y), ((x+w), (y+h)), (0,0,255))
+
+cv2.imshow('Image',laneDetect)
 cv2.waitKey(0)
